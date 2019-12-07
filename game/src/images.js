277f3f081bb734330445b05552dd08947cfd0ecf
Juan Diego Gutierrez <JuanD1425@me.com>
2019-12-06 19:05:44 -0500

    ifinished

---
+++ game/src/images.js
@@ -0,0 +1,29 @@
+import eleanor from './images/eleanor.jpg';
+import chidi from './images/chidi.jpg';
+import tahani from './images/tahani.jpg';
+import jason from './images/jason.jpg';
+import michael from './images/michael.jpg';
+import shawn from './images/shawn.jpg';
+import mindy_st_claire from './images/mindy-st-claire.jpg';
+import disco_janet from './images/disco_janet.png';
+import neutral_janet from './images/neutral_janet.png';
+import bad_janet from './images/bad_janet.jpg';
+import good_janet from './images/good_janet.jpg';
+import the_judge from './images/the_judge.png';
+
+const images = [
+    eleanor,
+    chidi,
+    tahani,
+    jason,
+    michael,
+    shawn,
+    mindy_st_claire,
+    disco_janet,
+    neutral_janet,
+    bad_janet,
+    good_janet,
+    the_judge
+];
+
+export default images;
