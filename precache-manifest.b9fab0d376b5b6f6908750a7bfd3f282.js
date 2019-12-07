3a3e79be7794f027d1ead088fa3ddc64754e46c1
Juan Diego Gutierrez <JuanD1425@me.com>
2019-12-06 19:49:42 -0500

    Updates

---
+++ precache-manifest.b9fab0d376b5b6f6908750a7bfd3f282.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658e637e1e2c4aa2b6cfd6b685174a01",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "6bf6fe5cafb803a6bfc6",
+    "url": "/ClickyGame/static/css/main.d62b3561.chunk.css"
+  },
+  {
+    "revision": "6475c955dd4c2b1fe979",
+    "url": "/ClickyGame/static/js/2.557ccbf2.chunk.js"
+  },
+  {
+    "revision": "6bf6fe5cafb803a6bfc6",
+    "url": "/ClickyGame/static/js/main.e8e63f6f.chunk.js"
+  },
+  {
+    "revision": "ef409265caa11495a58a",
+    "url": "/ClickyGame/static/js/runtime-main.33a53e43.js"
+  },
+  {
+    "revision": "625eb76a96df790ddcebca996a117372",
+    "url": "/ClickyGame/static/media/bad_janet.625eb76a.jpg"
+  },
+  {
+    "revision": "50b0039a0a9f5512a1386ab9660e1b13",
+    "url": "/ClickyGame/static/media/chidi.50b0039a.jpg"
+  },
+  {
+    "revision": "662caab4231db6c3d12335b4eb0fa70c",
+    "url": "/ClickyGame/static/media/disco_janet.662caab4.png"
+  },
+  {
+    "revision": "85c88304b3da2a71241c074ee3508bd6",
+    "url": "/ClickyGame/static/media/eleanor.85c88304.jpg"
+  },
+  {
+    "revision": "d3f36ab8a8b209e0c9022888dcda321b",
+    "url": "/ClickyGame/static/media/good_janet.d3f36ab8.jpg"
+  },
+  {
+    "revision": "5a6c70ee15099ce0b4f6505efbb37dae",
+    "url": "/ClickyGame/static/media/jason.5a6c70ee.jpg"
+  },
+  {
+    "revision": "9b39574f0365aba6deacf453d4eec30b",
+    "url": "/ClickyGame/static/media/michael.9b39574f.jpg"
+  },
+  {
+    "revision": "11dfcb1287bfe2f857837926eaea6d80",
+    "url": "/ClickyGame/static/media/mindy-st-claire.11dfcb12.jpg"
+  },
+  {
+    "revision": "6800c0331e8823c614d928ee44d1496a",
+    "url": "/ClickyGame/static/media/neutral_janet.6800c033.png"
+  },
+  {
+    "revision": "f2785f2a64721d71360748393520e430",
+    "url": "/ClickyGame/static/media/shawn.f2785f2a.jpg"
+  },
+  {
+    "revision": "0dd8017420c60af0cc37e0b04aadb6e9",
+    "url": "/ClickyGame/static/media/tahani.0dd80174.jpg"
+  },
+  {
+    "revision": "b6915e09646c708610c0474f5cdb96f8",
+    "url": "/ClickyGame/static/media/the_judge.b6915e09.png"
+  }
+]);
