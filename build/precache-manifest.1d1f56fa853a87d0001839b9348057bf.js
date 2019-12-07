87b938f483a67705bc278ccd5ac67ff79631b05e
Juan Diego Gutierrez <JuanD1425@me.com>
2019-12-06 19:38:09 -0500

    finished again

---
+++ build/precache-manifest.1d1f56fa853a87d0001839b9348057bf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4adc453e1bdc388177a927b9e88c6bcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bf6fe5cafb803a6bfc6",
+    "url": "/static/css/main.d62b3561.chunk.css"
+  },
+  {
+    "revision": "6475c955dd4c2b1fe979",
+    "url": "/static/js/2.557ccbf2.chunk.js"
+  },
+  {
+    "revision": "6bf6fe5cafb803a6bfc6",
+    "url": "/static/js/main.e8e63f6f.chunk.js"
+  },
+  {
+    "revision": "bcfe8bdc4ee2cee0cc4c",
+    "url": "/static/js/runtime-main.5a9bf512.js"
+  },
+  {
+    "revision": "625eb76a96df790ddcebca996a117372",
+    "url": "/static/media/bad_janet.625eb76a.jpg"
+  },
+  {
+    "revision": "50b0039a0a9f5512a1386ab9660e1b13",
+    "url": "/static/media/chidi.50b0039a.jpg"
+  },
+  {
+    "revision": "662caab4231db6c3d12335b4eb0fa70c",
+    "url": "/static/media/disco_janet.662caab4.png"
+  },
+  {
+    "revision": "85c88304b3da2a71241c074ee3508bd6",
+    "url": "/static/media/eleanor.85c88304.jpg"
+  },
+  {
+    "revision": "d3f36ab8a8b209e0c9022888dcda321b",
+    "url": "/static/media/good_janet.d3f36ab8.jpg"
+  },
+  {
+    "revision": "5a6c70ee15099ce0b4f6505efbb37dae",
+    "url": "/static/media/jason.5a6c70ee.jpg"
+  },
+  {
+    "revision": "9b39574f0365aba6deacf453d4eec30b",
+    "url": "/static/media/michael.9b39574f.jpg"
+  },
+  {
+    "revision": "11dfcb1287bfe2f857837926eaea6d80",
+    "url": "/static/media/mindy-st-claire.11dfcb12.jpg"
+  },
+  {
+    "revision": "6800c0331e8823c614d928ee44d1496a",
+    "url": "/static/media/neutral_janet.6800c033.png"
+  },
+  {
+    "revision": "f2785f2a64721d71360748393520e430",
+    "url": "/static/media/shawn.f2785f2a.jpg"
+  },
+  {
+    "revision": "0dd8017420c60af0cc37e0b04aadb6e9",
+    "url": "/static/media/tahani.0dd80174.jpg"
+  },
+  {
+    "revision": "b6915e09646c708610c0474f5cdb96f8",
+    "url": "/static/media/the_judge.b6915e09.png"
+  }
+]);
